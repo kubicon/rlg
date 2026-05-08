b3e927b718ff32880930632ab85ce2904f6e75a3
kubicon <kubicon3@fel.cvut.cz>
2026-05-08 10:23:48 -0400

    Added neurd

---
+++ src/losses/neurd.py
@@ -0,0 +1,40 @@
+import jax
+import jax.numpy as jnp
+
+def neurd_loss(
+  logits: jax.Array,
+  legal_actions: jax.Array,
+  advantages: jax.Array,
+  clip: float = 10000,
+  threshold: float = 2.0,
+) -> jax.Array:
+  """
+  Computes a Neural Replicator Dynamics (NeuRD) Loss.
+  It is same as replicator dynamics, but it forces the difference between any 2 logits for the same input to be at most 2 * threshold.
+  References:
+
+  [1] https://arxiv.org/abs/1906.00190
+  """
+  # logit_mean = jnp.sum(logits * action_mask, axis=-1, keepdims=True) / jnp.sum(
+  #   action_mask, axis=-1, keepdims=True
+  # )
+
+  # logits_centered = logits - logit_mean
+  # logits_centered = logits_centered * action_mask
+
+  logits_centered = logits * legal_actions
+
+  advantages = jnp.clip(advantages, -clip, clip)
+
+  logit_can_decrease = logits_centered > -threshold
+  logit_can_increase = logits_centered < threshold
+  positive_advantages = jnp.maximum(0.0, advantages)
+  negative_advantages = jnp.minimum(0.0, advantages)
+
+  advantage_weights = (
+    logit_can_decrease * negative_advantages + logit_can_increase * positive_advantages
+  )
+
+  loss = logits_centered * jax.lax.stop_gradient(advantage_weights)
+  loss = jnp.sum(loss * legal_actions, axis=-1)
+  return loss
